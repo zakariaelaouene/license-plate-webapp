ebe966ad0b93e3b2a0728c0c450d345321cfcb7a
zakariaelaouene <z.elaouene@aui.ma>
2023-04-28 20:00:57 +0100

    front fix

---
+++ front/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header';
+import LicensePlateDashboard from './components/LicensePlateDashboard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <LicensePlateDashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+function Header() {
+  return (
+    <header>
+      <h1>License Plate Dashboard</h1>
+      <SearchBar />
+    </header>
+  );
+}
+
+export default Header;
